1653016346e68bb900559c46652f4c6d85f03a18
Marco Karpf <marco.karpf@swisscom.com>
2023-01-24 22:57:37 +0100

    implement sql query in repos

--- src/main/java/ch/bbw/km/hotelbooking/repository/BookingRepository.java
+++ src/main/java/ch/bbw/km/hotelbooking/repository/BookingRepository.java
@@ -3,5 +3,8 @@ package ch.bbw.km.hotelbooking.repository;
 import ch.bbw.km.hotelbooking.model.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
+    List<Booking> findByUserEmail(String email);
 }
--- src/main/java/ch/bbw/km/hotelbooking/repository/UserRepository.java
+++ src/main/java/ch/bbw/km/hotelbooking/repository/UserRepository.java
@@ -4,4 +4,8 @@ import ch.bbw.km.hotelbooking.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    User getUserByEmail(String email);
+
+    User findByEmailAndPassword(String email, String password);
 }
